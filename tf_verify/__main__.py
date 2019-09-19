Enrich parser to handle Softmax and fix finding of output tensor

tf_verify/__main__.py
@@ -96,6 +96,8 @@ if(dataset=='acasxu'):
 else:
     print("netname ", netname, " epsilon ", epsilon, " domain ", domain, " dataset ", dataset, "args complete ", args.complete, " complete ",complete, " timeout_lp ",args.timeout_lp)
 
+non_layer_operation_types = ['NoOp', 'Assign', 'Const', 'RestoreV2', 'SaveV2', 'PlaceholderWithDefault', 'IsVariableInitialized', 'Placeholder', 'Identity']
+
 if is_saved_tf_model or is_pb_file:
     netfolder = os.path.dirname(netname)
 
@@ -112,7 +114,10 @@ if is_saved_tf_model or is_pb_file:
             sess.graph.as_default()
             tf.graph_util.import_graph_def(graph_def, name='')
     ops = sess.graph.get_operations()
-    eran = ERAN(sess.graph.get_tensor_by_name(ops[-1].name + ':0'), sess)
+    last_layer_index = -1
+    while ops[last_layer_index].type in non_layer_operation_types:
+        last_layer_index -= 1
+    eran = ERAN(sess.graph.get_tensor_by_name(ops[last_layer_index].name + ':0'), sess)
 
 else:
     if(dataset=='mnist'):

tf_verify/tensorflow_translator.py
@@ -101,13 +101,14 @@ class TFTranslator:
 		operation_resources = []
 		reshape_map = {}
 		operations_to_be_ignored = ["Reshape", "Pack", "Shape", "StridedSlice", "Prod", "ConcatV2"]
-		
+		operations_to_be_ignored_without_reshape = ["NoOp", "Assign", "Const", "RestoreV2", "SaveV2", "IsVariableInitialized", "Identity"]
+
 		with tf.Graph().as_default() as graph:
 			with tf.Session() as sess:
 				self.sess = sess
 				tf.import_graph_def(self.graph_def)
 				for op in graph.get_operations():
-					if op.type == "Const":
+					if op.type in operations_to_be_ignored_without_reshape:
 						continue
 					elif op.type in operations_to_be_ignored:
 						input_name  = op.inputs[0].name
@@ -164,11 +165,11 @@ class TFTranslator:
 						deeppoly_res =  (image_shape, window_size, in_out_info[2]) + in_out_info
 						deepzono_res = (image_shape, window_size, strides, padding) + in_out_info
 						operation_resources.append({'deepzono':deepzono_res, 'deeppoly':deeppoly_res})
-					elif op.type == "Placeholder":
+					elif op.type in ["Placeholder", "PlaceholderWithDefault"]:
 						deeppoly_res = in_out_info
 						deepzono_res = in_out_info
 						operation_resources.append({'deepzono':deepzono_res, 'deeppoly':deeppoly_res})
-					elif op.type in ["Relu", "Sigmoid", "Tanh"]:
+					elif op.type in ["Relu", "Sigmoid", "Tanh", "Softmax"]:
 						deeppoly_res = self.nonlinearity_resources(op) + in_out_info
 						deepzono_res = deeppoly_res
 						operation_resources.append({'deepzono':deepzono_res, 'deeppoly':deeppoly_res})