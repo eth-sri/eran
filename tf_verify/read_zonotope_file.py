ebc67ee6279bcba0383ad38135489b94c5397380
maurerjo <maurerjo@student.ethz.ch>
2019-09-20 20:48:08 +0200

    Make Zonotope read more accessable

--- tf_verify/read_zonotope_file.py
+++ tf_verify/read_zonotope_file.py
@@ -6,11 +6,11 @@ def read_zonotope(zonotope_file):
     assert os.path.isfile(zonotope_file), 'There exists no zonotope file.'
 
     zonotope_read = open(zonotope_file,'r').read()
-    zonotope = re.split(', |\n', zonotope_read)
-    zonotope_width = int(zonotope[0])
-    zonotope_height = int(zonotope[1])
-    
+    zonotope_read = zonotope_read.replace(' ', '')
+    zonotope = re.split(',|\n', zonotope_read)
+    zonotope_height = int(zonotope[0])
+    zonotope_width = int(zonotope[1])
+
     zonotope = [np.float64(x) for x in zonotope[2:zonotope_width*zonotope_height+2]]
     zonotope = np.array(zonotope)
-    print(zonotope)
     return np.reshape(zonotope, (zonotope_height, zonotope_width))
