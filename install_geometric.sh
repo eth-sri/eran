16ed2a35e4c6515fed4438ba3ec29d0ac4368bff
maurerjo <maurerjo@student.ethz.ch>
2020-01-09 11:37:30 +0100

    update geometric installation script to use Gurobi 9

--- install_geometric.sh
+++ install_geometric.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd gurobi811/linux64/src/build
+cd gurobi900/linux64/src/build
 sed -ie 's/^C++FLAGS =.*$/& -fPIC/' Makefile
 make
 cp libgurobi_c++.a ../../lib/
-cp ../../lib/libgurobi81.so /usr/lib
+cp ../../lib/libgurobi90.so /usr/lib
 cd ../../../..
 
-export GUROBI_HOME="$(pwd)/gurobi811/linux64"
+export GUROBI_HOME="$(pwd)/gurobi900/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:${GUROBI_HOME}/lib
 
