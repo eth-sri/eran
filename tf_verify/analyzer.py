eb53976eaee25b1559f80361d30c2c6d00e8f4eb
Gagandeep Singh <gsingh@student.ethz.ch>
2020-12-20 00:52:01 -0800

    get expr for output layer in terms of intermediate neurons

--- tf_verify/analyzer.py
+++ tf_verify/analyzer.py
@@ -210,6 +210,8 @@ class Analyzer:
         label_failed = []
         x = None
         if self.output_constraints is None:
+            #linexpr = get_output_uexpr_defined_over_previous_layers(self.man, element, 7, 5)
+            #elina_linexpr0_print(linexpr,None)
             candidate_labels = []
             if self.label == -1:
                 for i in range(output_size):
