6ec3fba2bc46dc14c5ac4b0c8af497ab642a2f5f
Mark Mueller <mark.mueller@inf.ethz.ch>
2021-04-11 10:37:44 +0200

    tf translator fix

--- tf_verify/tensorflow_translator.py
+++ tf_verify/tensorflow_translator.py
@@ -207,7 +207,7 @@ class TFTranslator:
 						operation_resources.append({'deepzono':deepzono_res, 'deeppoly':deeppoly_res})
 					elif op.type == "MaxPool" or op.type == "AvgPool":
 						image_shape, window_size, strides, pad_top, pad_left, pad_bottom, pad_right = self.pool_resources(op)
-						deeppoly_res =  (image_shape, window_size, strides, pad_top, pad_left) + in_out_info
+						deeppoly_res =  (image_shape, window_size, strides, pad_top, pad_left, pad_bottom, pad_right) + in_out_info
 						deepzono_res = deeppoly_res
 						operation_resources.append({'deepzono':deepzono_res, 'deeppoly':deeppoly_res})
 					elif op.type in ["Placeholder", "PlaceholderWithDefault"]:
